10910657b5b3f055b9f27a45908a4c5adb92a9dc
Christian <cwooddel@u.rochester.edu>
2021-02-15 11:24:55 -0500

    Create more controllers

--- redditclone/app/controllers/application_controller.rb
+++ redditclone/app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
-
+    helper_method :signed_in?, :current_user
     def current_user
-        @current_user ||= user.find_by(session_token: session[:session_token])
+        @current_user ||= User.find_by(session_token: session[:session_token])
     end
 
     def signed_in?
